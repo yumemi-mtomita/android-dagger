529bb4a10995658f50a8af09c9d9cca9562ab44a
富田 柾生 <m_tomita@yumemi.co.jp>
2020-05-21 19:06:52 +0900

    Login周りのリファクタリング

--- app/src/main/java/com/example/android/dagger/di/AppComponent.kt
+++ app/src/main/java/com/example/android/dagger/di/AppComponent.kt
@@ -1,6 +1,7 @@
 package com.example.android.dagger.di
 
 import android.content.Context
+import com.example.android.dagger.login.LoginComponent
 import com.example.android.dagger.main.MainActivity
 import com.example.android.dagger.registration.RegistrationComponent
 import dagger.BindsInstance
@@ -17,6 +18,7 @@ interface AppComponent {
     }
 
     fun registrationComponent(): RegistrationComponent.Factory
+    fun loginComponent(): LoginComponent.Factory
 
     fun inject(activity: MainActivity)
 }
--- app/src/main/java/com/example/android/dagger/di/AppSubcomponents.kt
+++ app/src/main/java/com/example/android/dagger/di/AppSubcomponents.kt
@@ -1,7 +1,8 @@
 package com.example.android.dagger.di
 
+import com.example.android.dagger.login.LoginComponent
 import com.example.android.dagger.registration.RegistrationComponent
 import dagger.Module
 
-@Module(subcomponents = [RegistrationComponent::class])
+@Module(subcomponents = [RegistrationComponent::class, LoginComponent::class])
 class AppSubcomponents
