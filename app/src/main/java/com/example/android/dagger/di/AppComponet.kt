1ad9f053c68f50b0e5532d577bd676102c42738d
富田 柾生 <m_tomita@yumemi.co.jp>
2020-05-21 13:11:38 +0900

    @Module, @Binds, and @BindsInstance annotations

--- app/src/main/java/com/example/android/dagger/di/AppComponet.kt
+++
@@ -1,9 +0,0 @@
-package com.example.android.dagger.di
-
-import com.example.android.dagger.registration.RegistrationActivity
-import dagger.Component
-
-@Component
-interface AppComponet {
-    fun inject(activity: RegistrationActivity)
-}
